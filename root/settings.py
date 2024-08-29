a979d555e402aa7316f9ced0931a961822ea7ed5
Mirshod <python3526@gmail.com>
2024-08-29 23:21:26 +0500

    sqlite

--- root/settings.py
+++ root/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": env('NAME'),
-        "USER": env('USER'),
-        "PASSWORD": env('PASSWORD'),
-        "HOST": env('HOST'),
-        "PORT": env('PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
